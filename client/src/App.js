e5b85b96d83c448778a12648b34f0e0d49cac0da
Emily Stewart <stewartemilym2014@gmail.com>
2021-06-26 13:07:16 -0700

    fixed website deploying issues

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,30 @@ import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
+
 function App() {
   return (
     <Router>
-      <>
+      <ApolloProvider client = {client}>
         <Navbar />
         <Switch>
           <Route exact path='/' component={SearchBooks} />
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
-      </>
+        </ApolloProvider>
     </Router>
   );
 }
